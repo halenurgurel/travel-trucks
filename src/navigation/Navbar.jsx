1df44dd0664208e63007b2a44e59061d74670803
Halenur Gurel <halenur.gurel@hotmail.com>
2026-04-20 15:21:54 +0300

    fix: navbar

--- src/navigation/Navbar.jsx
+++ src/navigation/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router";
 
 const Navbar = () => {
   return (
-    <div className="bg-bg-lighter border-bg-light flex justify-between border-b px-16 py-6">
+    <div className="bg-bg-lighter border-bg-light flex items-center justify-between border-b px-10 py-6">
       <Link to="/">
         <img src={logo} alt="TravelTrucks" />
       </Link>
